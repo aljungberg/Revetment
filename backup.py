74f9300979e8066f89e0a84fa7271cc8548a735d
Alexander Ljungberg <aljungberg@slevenbits.com>
2015-10-14 16:28:13 +0100

    New: newline separated includes and excludes.
    This makes it easy to specify patterns with spaces in them.
    
    Also prefix all files in archive with /b/ to work around "./cat" vs "cat" style non-normalised paths in Attic.

--- backup.py
+++ backup.py
@@ -1,6 +1,20 @@
 #!/usr/bin/env python3
 
-# Use Python to ensure we quote all arguments properly even when they come from env vars and contain spaces.
+"""
+Attic wrapper which handles excludes and includes as a new-line separated list
+of patterns (to allow spaces in names), and which normalises and relativises path 
+names. The normalisation ensures that the CWD is considered the "root" of the archive
+and gives each archived file a more canonical name.
+
+This works around a problem in Attic where it doesn't normalise paths. Consider a FS
+with "cat.txt" in it. If you back up "./", Attic will think of this file as "./cat.txt"
+and apply exclude patterns based on that. But on the other hand if you actually back
+up "cat.txt", Attic will think of this file as just that, "cat.txt". And if you back up
+"./cat.txt" it will /also/ think of it as "cat.txt" -- there is no way to get it to
+think of it as what you'd get with ".".
+
+So for consistency we always use absolute paths from the backup root (/b/).
+""" 
 
 import shlex
 import os
@@ -8,14 +22,14 @@ import sys
 import datetime
 
 def strip_paths(paths):
-    return [os.path.normpath("./" + l) for l in shlex.split(paths)]
+    """Ensure all paths are normalised and begin with '/b/'."""
+    return (os.path.normpath("/b/" + path) for path in paths if len(path))
 
 repo = os.path.join(os.getenv("SSHFS_MOUNT"), os.getenv("BACKUP_NAME", "backup.attic"))
 
 if sys.argv[1] == 'check':
     args = [
         "check",
-        "-v",
         repo
     ]
 elif sys.argv[1] == 'list':
@@ -49,10 +63,10 @@ elif sys.argv[1] == 'create':
         "%s::%s" % (repo, name)  
     ]
 
-    for exclude in strip_paths(os.getenv("EXCLUDES")):
+    for exclude in strip_paths(os.getenv("EXCLUDES").split("\n")):
         args += ["--exclude", exclude]
 
-    args += strip_paths(os.getenv("BACKUP_PATHS"))
+    args += list(strip_paths(os.getenv("BACKUP_PATHS").split("\n")))
 
 # sys.stdout.write("cwd: %s args: %r\n" % (os.getcwd(), args))
 # sys.stdout.flush()
--- backup.sh
+++ backup.sh
@@ -1,3 +1,5 @@
+#!/bin/bash
+
 SSH_PATH="3753@usw-s003.rsync.net:test/"
 SSH_KNOWN_HOSTS="$PWD/test/known_hosts"
 SSH_ID="$PWD/test/id_rsa"
@@ -5,6 +7,23 @@ BACKUP_ROOT="$PWD/test/backup/"
 ATTIC_KEYS="$PWD/test/attic"
 ATTIC_CACHE="$PWD/test/cache"
 KEEP_DAILY=2
+# What to back up. One name per line. Use ./ to back up everything in BACKUP_ROOT.
+BACKUP_PATHS="
+."
+# What to exclude. One exclude per line. Attic style wildcards allowed.
+EXCLUDES="
+/proc
+/dev
+/tmp
+/var/tmp
+/var/log
+/var/cache
+/media
+/lost+found
+"
+
+
+set -xe
 
 BACKUP_MOUNT_FLAGS="ro"
 
@@ -23,8 +42,8 @@ docker run \
     -v "$ATTIC_KEYS":/root/.attic:rw \
     -v "$ATTIC_CACHE":/root/.cache:rw \
     -v "$BACKUP_ROOT":/b:"$BACKUP_MOUNT_FLAGS" \
-    -e KEEP_DAILY="$KEEP_DAYS" \
+    -e KEEP_DAILY="$KEEP_DAILY" \
     -e SSH_PATH="$SSH_PATH" \
-    -e BACKUP_PATHS="1.txt \"path with space\"/dog.txt proc" \
-    -e EXCLUDES="/proc /dev /tmp /var/tmp /var/log /var/cache /media /lost+found" \
+    -e BACKUP_PATHS="$BACKUP_PATHS" \
+    -e EXCLUDES="$EXCLUDES" \
     backuper run.sh $@
